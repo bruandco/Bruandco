17d5c3622f153b4b6351074b007c2bcbba4d50ee
Bryan <bryanf@free.fr>
2012-07-07 02:20:54 +0200

    Starting block
    Debut

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>                                                      
+        <title></title>
+        <link rel="stylesheet" name="base" href="css/general.css" type="text/css" media="all" />
+        <link rel="stylesheet" name="base" href="css/overcast/jquery-ui-1.8.21.custom.css" type="text/css" media="all" />
+        <script type="text/javascript" href="javascript/jquery-1.7.2.min.js"></script>
+        <script type="text/javascript" href="javascript/jquery-ui-1.8.21.custom.min.js"></script>
+    </head>
+    <body>
+        <div class="logo" id="logo"></div>
+        <div class="lien_installation">
+            <a href="#">Commencer</a>
+        </div>
+        
+        
+        <div id="etape_installation">
+            <ul>
+                <li>
+                    <span>1</span>
+                    <span>Informations du site</span>
+                </li>
+                <li>
+                    <span>2</span>
+                    <span>Base de données</span>
+                </li>
+                <li>
+                    <span>3</span>
+                    <span>Utilisateurs</span>
+                </li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+    </body>
+</html>
+
